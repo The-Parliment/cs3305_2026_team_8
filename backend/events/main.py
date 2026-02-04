84bf0c737abe96814235de9307dbb25746d13df0
Roisin <roromaryquinn@gmail.com>
2026-02-04 13:01:04 +0000

    Created API endpoints for events service

--- backend/events/main.py
+++ backend/events/main.py
@@ -11,18 +11,27 @@ app = FastAPI(root_path="/events", title="events_service")
 
 # all placeholder code until it is possible to work with db
 # code is currently so vague it is identical
+# all placeholder code until it is possible to work with db
+# code is currently so vague it is identical
 
 """
 def get_current_user(authorization: str =  Header(...)) -> int:
     this is where we would call cillians verify_and_decode function.
     to extract the user_id from the JWToken.
+    this is where we would call cillians verify_and_decode function.
+    to extract the user_id from the JWToken.
 
+    Note - the issue at the moment is cillian's token does not contain a user_id
     Note - the issue at the moment is cillian's token does not contain a user_id
     because the db work has to be done first...
     would also be nice if we packed in the username/handle into that token too.
     would prevent a lot of cross-service chatter
+    would also be nice if we packed in the username/handle into that token too.
+    would prevent a lot of cross-service chatter
     return 1234
 """
+
+events = []
 
 events = []
 
@@ -38,74 +47,107 @@ def get_event_id() -> int:
 @app.get("/")
 async def root():
     return {"message": "Events Service API called"}
+    return {"message": "Events Service API called"}
 
 #There is absolute no HTML served in the microservice, this returns a token
-@app.post("/create", response_class = CreateResponse)
-async def create_item(inbound: CreateRequest, event_id: int= Depends(get_event_id)) -> CreateResponse:
+@app.post("/create")
+async def create_item(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
     logger.info(f"Create endpoint called for Event: {inbound.title}")
     create_response = CreateResponse( 
-        event_id = event_id,
+        event_id = user_id,
         title = inbound.title,
         description = inbound.description,
         datetime_start = inbound.datetime_start,
         datetime_end = inbound.datetime_end,
         venue = inbound.venue)
-    events.append(create_response) # add to database
+    events.append(create_response)
     return create_response
 
-@app.get("/eventinfo", response_class = InfoResponse)
-async def event_info(request: Request) -> InfoResponse:
-    return InfoResponse() #to be filled out later
+@app.post("/eventinfo")
+async def event_info(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    event_info_response = CreateResponse( 
+        event_id = user_id, # this is currently invalid
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
+    return event_info_response
 
 
-@app.get("/search", response_class = SearchResponse)
-async def search_events(request:Request) -> SearchResponse:
-    return SearchResponse(SearchEvent(), SearchEvent(), SearchEvent()) # needs to be filled out + restructured for scaling
-    
+@app.post("/search")
+async def search_events(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    search_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
+    return search_response 
 
-@app.post("/rsvp", response_class = RSVPResponse)
-async def rsvp_events(inbound: RSVPRequest) -> RSVPResponse:
-    rsvp_response = RSVPResponse( 
-        message="foodwise wuz here",
-        status="pending"
-        )
+@app.post("/rsvp")
+async def rsvp_events(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    rsvp_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
     return rsvp_response 
 
-@app.post("/invitecircle", response_class = InviteResponse)
-async def invite_circle(inbound: InviteRequest) -> InviteResponse:
-    invite_circle_response = InviteResponse( 
-        message="Hello everypony",
-        )
+@app.post("/invitecircle")
+async def invite_circle(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    invite_circle_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
     return invite_circle_response 
 
-@app.post("/invitegroup", response_class = InviteResponse)
-async def invite_group(inbound: InviteRequest) -> InviteResponse:
-    invite_group_response = InviteResponse( 
-        message="Hi single pony",
-        )
+@app.post("/invitegroup")
+async def invite_group(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    invite_group_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
     return invite_group_response 
 
-@app.post("/cancel", response_class = CancelResponse)
-async def cancel_event(inbound: CancelRequest) -> CancelResponse:
-    cancel_event_response = CancelResponse( 
-        message="No way!",
-        status="cancelled")
+@app.post("/cancel")
+async def cancel_event(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    cancel_event_response = CreateResponse ( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
     return cancel_event_response
 
-@app.put("/edit", response_class = InfoResponse)
-async def edit_event(inbound: EditRequest, event_id: int =Depends(get_event_id), venue: int = Depends(get_venue_id)) -> InfoResponse:
-    edit_event_response = InfoResponse( 
-        event_id=event_id,
-        title=inbound.title,
-        description=inbound.description,
-        datetime_start=inbound.datetime_start,
-        datetime_end=inbound.datetime_end,
-        latitude=inbound.latitude,
-        longitude=inbound.longitude,
-        venue_id=venue)
+@app.post("/edit")
+async def edit_event(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    edit_event_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
     return edit_event_response
 
-
-@app.get("/myevents", response_class = MyEventsResponse)
-async def my_events(request:Request) -> MyEventsResponse:
-    return MyEventsResponse(CreateResponse(), CreateResponse(), CreateResponse())
+@app.post("/myevents")
+async def my_events(inbound: CreateRequest, user_id: int = Depends(get_current_user), response_model = CreateResponse) -> CreateResponse:
+    my_events_response = CreateResponse( 
+        event_id = user_id,
+        title = inbound.title,
+        description = inbound.description,
+        datetime_start = inbound.datetime_start,
+        datetime_end = inbound.datetime_end,
+        venue = inbound.venue)
+    return my_events_response
