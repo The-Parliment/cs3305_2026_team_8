a11f268a14818bd011d588e4b93095759cc6c17c
Roisin <roromaryquinn@gmail.com>
2026-02-07 17:28:53 +0000

    added to API endpoint skeleton for events

--- backend/events/events_model.py
+++ backend/events/events_model.py
@@ -16,3 +16,59 @@ class CreateResponse(BaseModel):
     datetime_end: datetime
     venue: str
 
+
+class InfoResponse(BaseModel):
+    event_id: int
+    title: str
+    description: str
+    datetime_start: datetime
+    datetime_end: datetime
+    latitude: float
+    longitude: float
+    venue_id: int
+
+class SearchEvent(BaseModel):
+    event_id: int
+    title: str
+    datetime_start: datetime
+    datetime_end: datetime
+    latitude: float
+    longitude: float
+    venue_id: int
+
+class SearchResponse(BaseModel):
+    events: list
+
+class RSVPRequest(BaseModel):
+    event_id: int
+    status: str
+
+class RSVPResponse(BaseModel):
+    message: str
+    status: str
+
+class InviteRequest(BaseModel):
+    event_id: int
+
+class InviteResponse(BaseModel):
+    message: str
+
+class CancelRequest(BaseModel):
+    event_id: int
+
+class CancelResponse(BaseModel):
+    message: str
+    status: str
+
+class EditRequest(BaseModel):
+    event_id: int
+    title: str
+    description: str
+    datetime_start: datetime
+    datetime_end: datetime
+    latitude: float
+    longitude: float
+    venue_id: int
+
+class MyEventsResponse():
+    events: list
