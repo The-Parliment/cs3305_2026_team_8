8ef578d1f4ed158311094f2ef7d8f257db75bc4b
eirninlovesyou <cillibean16@gmail.com>
2026-02-15 17:19:06 +0000

    Created HTML request and response structures

--- backend/auth/structures.py
+++ backend/auth/structures.py
@@ -1,13 +1,43 @@
 from pydantic import BaseModel
 
+# Requests
+
+class RegisterRequest(BaseModel):
+    username: str
+    password: str
+    email : str
+    phone_number : str | None = None
+
 class LoginRequest(BaseModel):
     username: str
     password: str
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
+    
+class UsernameRequest(BaseModel):
+    username: str
+    
+class ResetPasswordRequest(BaseModel):
+    old_password: str
+    new_password: str
+
+# Responses
 
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
     refresh_token: str | None = None
 
-class RefreshRequest(BaseModel):
-    refresh_token: str
+class MessageResponse(BaseModel):
+    message: str
+
+class UsernameResponse(BaseModel):
+    username: str
+
+class UserDetailsResponse(BaseModel):
+    username: str
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str
+    phone_number: str | None = None
